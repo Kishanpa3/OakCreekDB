a45dde0c19f0461bc7b02768a1efe93977328a44
Ubuntu <ubuntu@ip-172-31-42-235.us-east-2.compute.internal>
2019-10-15 19:46:20 +0000

    [Added] - Animals App

---
+++ animal-app/config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'animals/index'
+  get 'animals/show'
+  get 'animals/new'
+  get 'animals/edit'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
