f0384c53dc53f2b1113e669e0d27a86bdebc1686
Kishan Patel <kishanpa@tamu.edu>
2019-10-18 21:48:43 +0000

    [Changed] - commonName Variable Name

--- animal-app/db/migrate/20191017155750_add_attributes_to_animals.rb
+++ animal-app/db/migrate/20191017155750_add_attributes_to_animals.rb
@@ -1,7 +1,7 @@
 class AddAttributesToAnimals < ActiveRecord::Migration[6.0]
   def change
     change_table :animals do |t|
-      t.rename :commonName, :common_name
+#      t.rename :commonName, :common_name
       t.rename :exhibit, :habitat_num
       t.column :species, :string
       t.column :sex, :string, limit: 6
