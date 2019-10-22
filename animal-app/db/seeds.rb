d677763506ba72e1ff9f6dca930c6e37d14b0c1f
Ubuntu <ubuntu@ip-172-31-33-27.us-east-2.compute.internal>
2019-10-22 21:24:46 +0000

    updated seed to generate names using names.txt

--- animal-app/db/seeds.rb
+++ animal-app/db/seeds.rb
@@ -12,16 +12,20 @@
 require 'securerandom' # used for generating a random hex tag - (https://stackoverflow.com/questions/88311/how-to-generate-a-random-string-in-ruby)
 
 puts 'STARTING TO SEED'
+  file = File.open("animals.txt")
+  list_of_animals = file.readlines.map(&:chomp)
+  file.close
+  file = File.open("names.txt")
+  list_of_names = file.readlines.map(&:chomp)
+  file.close
+  
   Animal.delete_all
   habitat = rand(10000..100000)
-  open('animals.txt') do |animals|
-    animals.read.each_line do |animal|
-      # puts animal
-      Animal.create(:habitat_num => habitat, :common_name => animal, :tag => SecureRandom.hex)
-      habitat = (habitat % 69 == 0) ? rand(10000..100000) : habitat + rand(10000..100000)
-    end
+  for i in 0..list_of_animals.length-1 do
+    Animal.create(:tag => SecureRandom.hex, :name => list_of_names[i], :common_name => list_of_animals[i], :habitat_num => habitat)
+    habitat = (habitat % 69 == 0) ? rand(10000..100000) : habitat + rand(10000..100000)
   end
 puts 'DONE SEEDING'
 
 Animal.create([   {habitat_num:"32", common_name:"Brown Bear", name:"Otto", tag: "BBO001", species: "Ursus Arctos", weight: 325, sex: "Male", neutered: "Yes", age: 3, dob: Time.new(2016,1,20) },
-                  {habitat_num:"30", common_name:"Raccoon", name:"Larry", tag: "RCC001"}])
+                  {habitat_num:"30", common_name:"Raccoon", name:"Larry", tag: "RCC001"}  ])
