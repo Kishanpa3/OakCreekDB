8b6f4b1c05d511cb25c597066fbfab6a843caaff
Ubuntu <ubuntu@ip-172-31-33-27.us-east-2.compute.internal>
2019-10-22 20:45:51 +0000

    updated seed to add 6k dummy entries

--- animal-app/db/seeds.rb
+++ animal-app/db/seeds.rb
@@ -6,4 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-animals = Animal.create([{habitat_num:"32", common_name:"Brown Bear"}, {habitat_num:"30", common_name:"Raccoon"}])
+# animals = Animal.create([{habitat_num:"32", common_name:"Brown Bear"}, {habitat_num:"30", common_name:"Raccoon"}])
+
+
+require 'securerandom' # used for generating a random hex tag - (https://stackoverflow.com/questions/88311/how-to-generate-a-random-string-in-ruby)
+
+puts 'STARTING TO SEED'
+  Animal.delete_all
+  habitat = rand(10000..100000)
+  open('animals.txt') do |animals|
+    animals.read.each_line do |animal|
+      # puts animal
+      Animal.create(:habitat_num => habitat, :common_name => animal, :tag => SecureRandom.hex)
+      habitat = (habitat % 69 == 0) ? rand(10000..100000) : habitat + rand(10000..100000)
+    end
+  end
+puts 'DONE SEEDING'
