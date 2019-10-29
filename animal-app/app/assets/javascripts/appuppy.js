e930dd89c4e0b8debf100cb0c91428a51324832b
Andrew Wong <26995525+andrewywong@users.noreply.github.com>
2019-10-29 13:46:02 +0000

    add multiple uppy sources

--- animal-app/app/assets/javascripts/appuppy.js
+++ animal-app/app/assets/javascripts/appuppy.js
@@ -1,48 +1,48 @@
-// Import the plugins
-const Uppy = require('@uppy/core')
-const AwsS3Multipart = require('@uppy/aws-s3-multipart')
-const Dashboard = require('@uppy/dashboard')
-const GoogleDrive = require('@uppy/google-drive')
-const Dropbox = require('@uppy/dropbox')
-const Facebook = require('@uppy/facebook')
-const Instagram = require('@uppy/instagram')
-const Webcam = require('@uppy/webcam')
+// // Import the plugins
+// const Uppy = require('@uppy/core')
+// const AwsS3Multipart = require('@uppy/aws-s3-multipart')
+// const Dashboard = require('@uppy/dashboard')
+// const GoogleDrive = require('@uppy/google-drive')
+// const Dropbox = require('@uppy/dropbox')
+// const Facebook = require('@uppy/facebook')
+// const Instagram = require('@uppy/instagram')
+// const Webcam = require('@uppy/webcam')
 
-// And their styles (for UI plugins)
-require('@uppy/core/dist/style.css')
-require('@uppy/dashboard/dist/style.css')
+// // // And their styles (for UI plugins)
+// // require('@uppy/core/dist/style.css')
+// // require('@uppy/dashboard/dist/style.css')
 
-const uppy = Uppy({
-  // debug: true,
-  autoProceed: false,
-  // restrictions: {
-  //   maxFileSize: 1000000,
-  //   maxNumberOfFiles: 3,
-  //   minNumberOfFiles: 2,
-  //   allowedFileTypes: ['image/*', 'video/*']
-  // }
-})
-.use(Dashboard, {
-  trigger: '.UppyModalOpenerBtn',
-  inline: false,
-  target: '.DashboardContainer',
-  replaceTargetContent: true,
-  showProgressDetails: true,
-  // note: 'Images and video only, 2–3 files, up to 1 MB',
-  height: 470,
-  metaFields: [
-    { id: 'name', name: 'Name', placeholder: 'file name' },
-    { id: 'caption', name: 'Caption', placeholder: 'describe what the image is about' }
-  ],
-  browserBackButtonClose: true
-})
-.use(GoogleDrive, { target: Dashboard, companionUrl: 'https://companion.uppy.io' })
-.use(Dropbox, { target: Dashboard, companionUrl: 'https://companion.uppy.io' })
-.use(Facebook, { target: Dashboard, companionUrl: 'https://companion.uppy.io' })
-.use(Instagram, { target: Dashboard, companionUrl: 'https://companion.uppy.io' })
-.use(Webcam, { target: Dashboard, facingMode: 'environment' })
+// const uppy = Uppy({
+//   // debug: true,
+//   autoProceed: false,
+//   // restrictions: {
+//   //   maxFileSize: 1000000,
+//   //   maxNumberOfFiles: 3,
+//   //   minNumberOfFiles: 2,
+//   //   allowedFileTypes: ['image/*', 'video/*']
+//   // }
+// })
+// .use(Dashboard, {
+//   trigger: '.UppyOpenerBtn',
+//   inline: false,
+//   target: '.DashboardContainer',
+//   replaceTargetContent: true,
+//   showProgressDetails: true,
+//   // note: 'Images and video only, 2–3 files, up to 1 MB',
+//   height: 470,
+//   metaFields: [
+//     { id: 'name', name: 'Name', placeholder: 'file name' },
+//     { id: 'caption', name: 'Caption', placeholder: 'describe what the image is about' }
+//   ],
+//   browserBackButtonClose: true
+// })
+// .use(GoogleDrive, { target: Dashboard, companionUrl: 'https://companion.uppy.io' })
+// .use(Dropbox, { target: Dashboard, companionUrl: 'https://companion.uppy.io' })
+// .use(Facebook, { target: Dashboard, companionUrl: 'https://companion.uppy.io' })
+// .use(Instagram, { target: Dashboard, companionUrl: 'https://companion.uppy.io' })
+// .use(Webcam, { target: Dashboard, facingMode: 'environment' })
 
-uppy.on('complete', result => {
-  console.log('successful files:', result.successful)
-  console.log('failed files:', result.failed)
-})
+// uppy.on('complete', result => {
+//   console.log('successful files:', result.successful)
+//   console.log('failed files:', result.failed)
+// })
