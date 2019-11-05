4297f06a12b356a7e38b9a1835a1e44a794bcfab
Andrew Wong <26995525+andrewywong@users.noreply.github.com>
2019-11-05 18:16:17 +0000

    Uncomment rand in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,7 +23,7 @@ puts 'STARTING TO SEED'
   rand_habitat = rand(10000..100000)
   for i in 0..list_of_animals.length-1 do
     Animal.create(:tag => SecureRandom.hex, :name => list_of_names[i], :common_name => list_of_animals[i], :habitat_num => rand_habitat, :dob => Time.new(2016,1,20))
-    #rand_habitat = (rand_habitat % 69 == 0) ? rand(10000..100000) : rand_habitat + rand(10000..100000)
+    rand_habitat = (rand_habitat % 69 == 0) ? rand(10000..100000) : rand_habitat + rand(10000..100000)
   end
 puts 'DONE SEEDING'
 
