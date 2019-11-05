c5a87251e1c62c76bc7215c4121a4e52d2832a65
Andrew Wong <26995525+andrewywong@users.noreply.github.com>
2019-11-05 18:50:32 +0000

    Test string

--- db/migrate/20191015185732_create_animals.rb
+++ db/migrate/20191015185732_create_animals.rb
@@ -1,7 +1,7 @@
 class CreateAnimals < ActiveRecord::Migration[6.0]
   def change
     create_table :animals do |t|
-      t.integer :exhibit
+      t.string :exhibit
       t.string :common_name
 
       t.timestamps
