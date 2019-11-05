bbb6944e35f872414a347732712a49d92213ca3c
Andrew Wong <26995525+andrewywong@users.noreply.github.com>
2019-11-05 00:19:26 +0000

    Modify schema

--- db/migrate/20191022202856_add_name_and_habitat_to_int.rb
+++ db/migrate/20191022202856_add_name_and_habitat_to_int.rb
@@ -2,7 +2,7 @@ class AddNameAndHabitatToInt < ActiveRecord::Migration[6.0]
   def change
     change_table :animals do |t|
       t.column :name, :string
-      t.change(:habitat_num, :string)
+      t.change(:habitat_num, :integer)
     end
   end
 end
