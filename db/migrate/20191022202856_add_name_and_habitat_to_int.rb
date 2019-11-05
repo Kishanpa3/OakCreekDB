c3f165eb5d51115292f609c5f27eae43be37584e
Andrew Wong <26995525+andrewywong@users.noreply.github.com>
2019-11-05 17:24:34 +0000

    Modify migration AddNameAndHabitatToInt

--- db/migrate/20191022202856_add_name_and_habitat_to_int.rb
+++ db/migrate/20191022202856_add_name_and_habitat_to_int.rb
@@ -1,8 +1,6 @@
 class AddNameAndHabitatToInt < ActiveRecord::Migration[6.0]
   def change
-    change_table :animals do |t|
-      t.column :name, :string
-      t.change(:habitat_num, :integer)
-    end
+    add_column :animals, :name, :string  
+    change_column :animals, :habitat_num, :integer
   end
 end
