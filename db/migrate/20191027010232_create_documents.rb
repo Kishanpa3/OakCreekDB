461adb82b4f675b404b0d6fdabf0b11d813a1bc7
Andrew Wong <26995525+andrewywong@users.noreply.github.com>
2019-11-07 14:43:43 +0000

    Add index to file migration

--- db/migrate/20191027010232_create_documents.rb
+++ db/migrate/20191027010232_create_documents.rb
@@ -1,7 +1,7 @@
 class CreateDocuments < ActiveRecord::Migration[6.0]
   def change
     create_table :documents do |t|
-      t.references :animal, foreign_key: true
+      t.references :animal, foreign_key: true, index: true
       t.text :file_data
 
       t.timestamps
