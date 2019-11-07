001be0a05569af2da831cf7fceda4c578cd58a78
JJ9897 <justinj9897@tamu.edu>
2019-11-07 17:58:32 +0000

    Ran devise migration and added basic user authentication as well as 'protect_from_forgery'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_18_165748) do
+ActiveRecord::Schema.define(version: 2019_11_07_174849) do
 
   create_table "animals", force: :cascade do |t|
     t.integer "habitat_num"
@@ -37,6 +37,22 @@ ActiveRecord::Schema.define(version: 2019_10_18_165748) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["animal_id"], name: "index_diets_on_animal_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "diets", "animals"
