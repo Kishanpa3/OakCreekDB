80e129ee73f8883fdc6d7fd537c5eebcdc075f84
JJ9897 <justinj9897@tamu.edu>
2019-11-12 23:34:47 +0000

    Implemented the (form) functionality of the 'Admin Dashboard' for both 'Approved' and 'Unapproved'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,5 +12,24 @@ class UsersController < ApplicationController
   end
 
   def update
+    if params[:users]
+      User.update(params[:users].keys, params[:users].values)
+      
+      # params[:users].each_pair do |user_id, attr_vals|
+      #   # puts user_id
+      #   # puts attr_vals
+      #   # puts attr_vals["admin"]
+      #   @user = User.find(user_id)
+      #   attr_vals.each_pair do |attribute, val|
+      #     # puts "#{attribute}=#{val}"
+      #     @user.send("#{attribute}=", val)
+      #     @user.save!
+          
+      #     # @user.update_column!(attribute, val)
+      #   end
+      # end
+    else
+      puts 'NO USERS PROVIDED'
+    end
   end
 end
