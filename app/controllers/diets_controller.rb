5ecc7d4e6b39376e52fc3dcde77bc4ab96eb0971
Ubuntu <ubuntu@ip-172-31-42-235.us-east-2.compute.internal>
2019-11-14 16:10:37 +0000

    [Added] - some files for diet page still WIP

--- app/controllers/diets_controller.rb
+++ app/controllers/diets_controller.rb
@@ -1,11 +1,36 @@
 class DietsController < ApplicationController
-    def diets_params
+    def diet_params
         params.require(:diet).permit(:instructions, :am, :pm, :dish, :animal_id)
+    end
+    
+    def new
+       @diet = Diet.new 
+       @diet.animal_id = @animal.id
+    end
+
+    def edit
     end
 
     def create
-        @diet = Diet.new(diets_params)
-        @diet.animal_id = current_animal.id
+        @diet = Diet.new(diet_params)
+        @diet.animal_id = @animal.id
+        
+        if @diet.save
+           redirect_to @animal
+        else
+            #render 'new'
+        end
     end
 
+    def update
+        @diet.update(diet_params)
+    end
+    
+    def destroy
+        @diet.destroy
+    end
+    
+    def set_animal
+       @animal = Animal.find(params[:animal_id]) 
+    end
 end
