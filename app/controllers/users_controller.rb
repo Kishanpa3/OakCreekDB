c53a371d82f555c8ed7ab8fd92b7e4c67442fe8b
JJ9897 <justinj9897@tamu.edu>
2019-11-22 04:04:53 +0000

    Setup basic partial request for modal for index.html.haml using javascript

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,22 +12,14 @@ class UsersController < ApplicationController
   def update
     if params[:users]
       User.update(params[:users].keys, params[:users].values)
-      
-      # params[:users].each_pair do |user_id, attr_vals|
-      #   # puts user_id
-      #   # puts attr_vals
-      #   # puts attr_vals["admin"]
-      #   @user = User.find(user_id)
-      #   attr_vals.each_pair do |attribute, val|
-      #     # puts "#{attribute}=#{val}"
-      #     @user.send("#{attribute}=", val)
-      #     @user.save!
-          
-      #     # @user.update_column!(attribute, val)
-      #   end
-      # end
     else
       puts 'NO USERS PROVIDED'
     end
+  end
+  
+  
+  def serve_index_partial
+    puts "REQUEST FOR PARTIAL #{params}"
+    render json: { html: render_to_string(partial: 'users/update', locals: {:user => params}) }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'users/index'
   get 'users/destroy'
   get 'users/update'
+  get 'users/index_partial', to: 'users#serve_index_partial', as: 'request_index_partial'
   devise_for :users, :controllers => { registrations: 'registrations' }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
