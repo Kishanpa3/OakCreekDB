d4203b7b8506be6ca2ae0d3aa23a69e5de5671ad
Ubuntu <ubuntu@ip-172-31-42-235.us-east-2.compute.internal>
2019-11-22 21:43:48 +0000

    [Added] - Worrking cucumber test for editDiet

--- features/step_definitions/editEntrySteps.rb
+++ features/step_definitions/editEntrySteps.rb
@@ -1,3 +1,4 @@
+require './features/support/helpers'
 
 When("I fill text field {string} with {string}") do |string, string2|
   fill_in(string, with: string2)
@@ -9,13 +10,3 @@ When("I press the button {string}") do |string|
 end
 
 
-When("I fill text area {string} with {string}") do |string, string2|
-  wait_for_ajax
-  #fill_in(string, with: string2)
-  within('#dish_area') do
-    el = find(:xpath, "//body[@class='wysihtml5 wysihtml5-editor']")
-    el.send_keys(string2)
-    expect(page).to have_content("Black Bowl")
-    expect(page).to have_content(string2)
-  end
-end
