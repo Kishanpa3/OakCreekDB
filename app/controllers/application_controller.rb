0d885bc636015904d46a4aea1f01b2d789b4313f
JJ9897 <justinj9897@tamu.edu>
2019-11-22 07:10:09 +0000

    Increased WEBbrick MAX_URI_LENGTH

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  WEBrick::HTTPUtils.const_set("MAX_URI_LENGTH", 10240)
   protect_from_forgery with: :exception
   before_action :authenticate_user!, except: :update_form_partial
 end
