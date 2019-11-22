ccada8edee416f4cfa786df56ad7e171ec1d35d6
JJ9897 <justinj9897@tamu.edu>
2019-11-22 07:12:57 +0000

    reverted previous commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  WEBrick::HTTPUtils.const_set("MAX_URI_LENGTH", 10240)
   protect_from_forgery with: :exception
   before_action :authenticate_user!, except: :update_form_partial
 end
