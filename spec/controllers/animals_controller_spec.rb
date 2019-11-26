8425dc22b57a5bc6a26a0fcb1d870150913d3b21
Ubuntu <ubuntu@ip-172-31-33-139.us-east-2.compute.internal>
2019-11-26 16:35:28 +0000

    [added] rspec tests

--- spec/controllers/animals_controller_spec.rb
+++ spec/controllers/animals_controller_spec.rb
@@ -29,7 +29,7 @@ RSpec.describe AnimalsController, type: :controller do
 
   describe "GET #edit" do
     it "returns http success" do
-      get :edit
+      get :edit, params: { id: @animal.id }
       expect(response).to have_http_status(:success)
     end
   end
