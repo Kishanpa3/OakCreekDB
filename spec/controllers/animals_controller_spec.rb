241092911e9dbb2526ca9ebc5ff2725020fdb87a
Ubuntu <ubuntu@ip-172-31-33-139.us-east-2.compute.internal>
2019-11-26 16:28:32 +0000

    [added] rspec tests

--- spec/controllers/animals_controller_spec.rb
+++ spec/controllers/animals_controller_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe AnimalsController, type: :controller do
 
+  before (:each) do
+    @animal = Animal.create(habitat_num:"32", common_name:"Brown Bear", name:"Otto", tag: "BBO001", species: "Ursus Arctos", weight: 325, sex: "Male", neutered: "Yes", age: 3, dob: Time.new(2016,1,20))
+  end
+  
   describe "GET #index" do
     it "returns http success" do
       get :index
@@ -11,7 +15,7 @@ RSpec.describe AnimalsController, type: :controller do
 
   describe "GET #show" do
     it "returns http success" do
-      get :show
+      get :show, params: { id: @animal.id }
       expect(response).to have_http_status(:success)
     end
   end
