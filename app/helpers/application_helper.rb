92e6bf79891dd2691cb95d0ded144fafbf40bc5e
JJ9897 <justinj9897@tamu.edu>
2019-11-29 06:24:08 +0000

    Moved alerts to partial and added helper function to prevent application.html content from being displayed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,11 @@ module ApplicationHelper
   def resource_class
     devise_mapping.to
   end
+  
+  def no_content_for(symbol)
+    # content can't be an empty string, so fill it with nbsp symbol
+    content_for(symbol) do 
+      ' &nbsp '
+    end  
+  end
 end
