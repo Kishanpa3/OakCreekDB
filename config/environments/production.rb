7949df21b6623512cffdf01fe75d6c4f5de78b9f
Ubuntu <26995525+andrewywong@users.noreply.github.com>
2019-12-06 03:56:21 +0000

    Change animal index page buttons to be mobile friendly

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -113,8 +113,8 @@ Rails.application.configure do
   
   
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => 'https://secret-eyrie-12615.herokuapp.com/'}
-  # config.action_mailer.default_url_options = { :host => 'https://radiant-brook-01872.herokuapp.com/'}
+  # config.action_mailer.default_url_options = { :host => 'https://secret-eyrie-12615.herokuapp.com/'}
+  config.action_mailer.default_url_options = { :host => 'https://radiant-brook-01872.herokuapp.com/'}
   
   
   ActionMailer::Base.delivery_method = :smtp
