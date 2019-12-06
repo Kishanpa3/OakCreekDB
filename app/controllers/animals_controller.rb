98a07fbd684e0bb30e993229762416616ba21f06
Ubuntu <26995525+andrewywong@users.noreply.github.com>
2019-12-06 22:26:36 +0000

    Start implementation of ajax prepend

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -43,6 +43,10 @@ class AnimalsController < ApplicationController
     # puts "ANIMAL PARAMS: #{animal_params.keys}"
     # puts "DOC ATR: #{animal_params[:documents_attributes]}"
     if (animal_params.key?(:documents_attributes))
+      respond_to do |format|
+        format.html { redirect_to animal_documents_path(@animal) }
+        format.js
+      end
     else
       flash[:notice] = "#{@animal.tag} was successfully updated."
       redirect_to animal_path(@animal)
