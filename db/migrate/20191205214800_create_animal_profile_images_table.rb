3d2a08ca60777adc7d7f3d664c976d8e50583843
Ubuntu <26995525+andrewywong@users.noreply.github.com>
2019-12-08 08:47:26 +0000

    Remove redundent null in timestamp

--- db/migrate/20191205214800_create_animal_profile_images_table.rb
+++ db/migrate/20191205214800_create_animal_profile_images_table.rb
@@ -3,7 +3,8 @@ class CreateAnimalProfileImagesTable < ActiveRecord::Migration[6.0]
     create_table :animal_profile_images do |t|
       t.references :animal,    null: false, foreign_key: true, index: true
       t.references :document,  null: false, foreign_key: true
-      t.timestamps null: false
+      
+      t.timestamps
     end
   end
 end
